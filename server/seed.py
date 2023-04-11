3211a24930e22d07ffb408edb3c6779a6e6f6435
Ash <ashjames.dean@gmail.com>
2023-04-11 13:13:37 -0400

    got the users to seed as well

--- server/seed.py
+++ server/seed.py
@@ -22,13 +22,22 @@ with app.app_context():
     print("creating birds...")
     birds = [
         Bird(common_name = 'Red-throated loon', scientific_name = 'Gavia stellata', 
-             body_color = ['black', 'gray', 'white'],  beak_color = 'black', eye_color = 'red',
+             body_color = 'black',  beak_color = 'black', eye_color = 'red',
              neck_color = 'red', sex = 'male', size = 24, swim = 'yes', season = 'winter_u'),
         Bird(common_name = 'Common loon', scientific_name = 'Gavia immer', 
-             body_color = ['black','white'],  beak_color = ['black','white'], eye_color = 'red',
-             neck_color = ['black','white'], sex = 'male', size = 27, swim = 'yes', 
-             season = ['winter_u', 'spring_u','earlyfall_r', 'latefall_f'])
+             body_color = 'black',  beak_color = 'black', eye_color = 'red',
+             neck_color = 'black', sex = 'male', size = 27, swim = 'yes', 
+             season = 'winter_u')
     ]
+    # birds = [
+    #     Bird(common_name = 'Red-throated loon', scientific_name = 'Gavia stellata', 
+    #          body_color = ['black', 'gray', 'white'],  beak_color = 'black', eye_color = 'red',
+    #          neck_color = 'red', sex = 'male', size = 24, swim = 'yes', season = 'winter_u'),
+    #     Bird(common_name = 'Common loon', scientific_name = 'Gavia immer', 
+    #          body_color = ['black','white'],  beak_color = ['black','white'], eye_color = 'red',
+    #          neck_color = ['black','white'], sex = 'male', size = 27, swim = 'yes', 
+    #          season = ['winter_u', 'spring_u','earlyfall_r', 'latefall_f'])
+    # ]
 
     db.session.add_all(birds)
 
